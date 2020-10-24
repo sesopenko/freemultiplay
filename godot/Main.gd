22a35d5463feab0ec13b4c54a6b9c1832f14faef
Sean Esopenko <sesopenko@gmail.com>
2020-10-24 12:45:29 -0600

    Run godot game in server mode if flag set.

--- godot/Main.gd
+++ godot/Main.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
+var _is_server_mode := false
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+const SERVER_FLAG = "--server"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Ready")
+	_sync_game_mode()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Synchronizes the game mode from command line arguments.
+func _sync_game_mode():
+	var args: PoolStringArray
+	args = OS.get_cmdline_args()
+	if SERVER_FLAG in args:
+		print("is server")
+		_is_server_mode = true
+		# No other functionality to provide yet, so quit to free resources.
+		get_tree().quit()
+	else:
+		print("is not server")
